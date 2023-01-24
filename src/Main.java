9d75b6b9ca0a8a76833079b531ac5afe0d4300f3
Sarita <saritaradia65@gmail.com>
2023-01-24 14:24:08 +0000

    Basic if statement formed;

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int x = 5;
-        int y = 7;
-        double z = 57;
-        double sum = x + y + z;
-        System.out.println(sum);
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        if (s.equals("Dog")) {
+            System.out.println("WOOF!");
+        }
+
 
     }
 }
