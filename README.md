7ca3d1a1987142d32a2224eeabd3fa3653e1bdc5
Sarita <saritaradia65@gmail.com>
2023-01-24 12:22:59 +0000

    Project set up

---
+++ README.md
@@ -0,0 +1,3 @@
+## Learning the basics of Java
+
+- Following the [Intro to Java Programming - Course for Absolute Beginners](https://www.youtube.com/watch?v=GoXwIVyNvX0) by freeCodeCamp
---
+++ src/Main.java
@@ -0,0 +1,23 @@
+// File: App.java
+
+public class Main {
+    public static void main(String[] args) {
+        int hello_world = 5;
+        System.out.println(hello_world);
+
+        double num2 = 5.0;
+        System.out.println(num2);
+
+        boolean b = false;
+        System.out.println(b);
+
+        char c = '9';
+        System.out.println(c);
+
+        String str = "tim";
+        System.out.println(str);
+
+        int tim = hello_world;
+        System.out.println(tim);
+    }
+}
