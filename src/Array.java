9b8b09f1ce4e040ca9dbe8aa971a5a282ed53b0f
Sarita <saritaradia65@gmail.com>
2023-01-25 11:50:13 +0000

    Integer array added

--- src/Array.java
+++ src/Array.java
@@ -5,8 +5,9 @@ import java.util.Scanner;
 public class Array {
     public static void main(String[] args) {
         String[] stringArr = {"one","two","three","four","five"}; 
- 
-        System.out.println(stringArr[2]);
+        System.out.println(stringArr[2]); // three
 
+        Integer[] intArr = {1,2,3,4,5};
+        System.out.println(intArr[1]); // 2
     }
 }
