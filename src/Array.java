c24cea3707a079301dbc28695cb1b2cc5644be74
Sarita <saritaradia65@gmail.com>
2023-01-25 11:47:32 +0000

    String array created

---
+++ src/Array.java
@@ -0,0 +1,19 @@
+// File: src/Array.java
+
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[] args) {
+        String[] stringArr = new String[5]; // String Array declared 
+
+        stringArr[0] = "one";
+        stringArr[1] = "two";
+        stringArr[2] = "three";
+        stringArr[3] = "four";
+        stringArr[4] = "five";
+        
+        String elementZero = stringArr[0];
+        System.out.println(elementZero);
+
+    }
+}
