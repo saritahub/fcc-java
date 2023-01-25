d50b1bacca25149af668a0a991dd4e64fb9023fe
Sarita <saritaradia65@gmail.com>
2023-01-25 11:49:02 +0000

    Refactor string array

--- src/Array.java
+++ src/Array.java
@@ -4,16 +4,9 @@ import java.util.Scanner;
 
 public class Array {
     public static void main(String[] args) {
-        String[] stringArr = new String[5]; // String Array declared 
-
-        stringArr[0] = "one";
-        stringArr[1] = "two";
-        stringArr[2] = "three";
-        stringArr[3] = "four";
-        stringArr[4] = "five";
-        
-        String elementZero = stringArr[0];
-        System.out.println(elementZero);
+        String[] stringArr = {"one","two","three","four","five"}; 
+ 
+        System.out.println(stringArr[2]);
 
     }
 }
