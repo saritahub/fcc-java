b155ba4861b239c36c4fec7989072fdfb8f19dbf
Sarita <saritaradia65@gmail.com>
2023-02-03 16:28:01 +0000

    HashMap example created

---
+++ src/HashMapExample.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapExample {
+    public static void main(String[] args) {
+        Map m = new HashMap<>();
+
+        m.put("First key", 1);
+        m.put("Second key", 2);
+        m.put("Third key", 3);
+        m.put("Fourth key", "four");
+
+        System.out.println(m);
+
+    }
+
+}
