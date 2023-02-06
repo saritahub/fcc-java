d180e06217b10be917cd69ba32fd761f2f60942a
Sarita <saritaradia65@gmail.com>
2023-02-06 11:37:02 +0000

    String locater created

---
+++ src/Locate.java
@@ -0,0 +1,6 @@
+public class Locate {
+    public static void main(String[] args) {
+        String txt = "Please locate where tea occurs!";
+        System.out.println(txt.indexOf("tea"));
+    }
+}
