b3207ead1314d3ad962be833b24d5d869ac310dc
Sarita <saritaradia65@gmail.com>
2023-02-18 11:45:47 +0000

    Refactor whitespace

--- src/VowelCount.java
+++ src/VowelCount.java
@@ -4,10 +4,7 @@ public class Vowels {
 
     public static int getCount(String str) {
         String[] stringArr = new String[] {str}; 
-        
-
         System.out.println(stringArr); 
-
     }
   
   }
